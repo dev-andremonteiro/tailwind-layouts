40724cf46877ee8335db571f284816edf16ca449
Andre Monteiro <andrevmonteiro.eng@gmail.com>
2023-06-17 23:43:55 -0400

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='container mx-auto'>
+      <div className='flex flex-row flex-wrap py-4'>
+        <div className='bg-red-600 w-10 h-10'/>
+      </div>
+    </div>
+  )
+}
+
+export default App
